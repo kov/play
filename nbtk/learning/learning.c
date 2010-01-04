c74afe19cda833a3e4f5a750cf43beba600ad33d
Gustavo Noronha Silva <gustavo.noronha@collabora.co.uk>
2010-01-04 19:32:55 -0200

    Add a ListView test

--- nbtk/learning/learning.c
+++ nbtk/learning/learning.c
@@ -16,13 +16,64 @@
 
 #include <clutter/clutter.h>
 #include <glib.h>
+#include <gio/gio.h>
 #include <nbtk/nbtk.h>
+
+enum {
+    COLUMN_NAME,
+    COLUMN_SIZE,
+
+    N_COLUMNS
+};
+
+static ClutterModel*
+create_model()
+{
+    ClutterModel *model;
+    GFile *directory;
+    GFileEnumerator *enumerator;
+    GFileInfo *info;
+    GError *error = NULL;
+
+    model = clutter_list_model_new(N_COLUMNS,
+                                   G_TYPE_STRING, "File name",
+                                   G_TYPE_UINT64, "Size (bytes)");
+
+    directory = g_file_new_for_path(g_get_home_dir());
+    enumerator = g_file_enumerate_children(directory,
+                                           "standard::*",
+                                           G_FILE_QUERY_INFO_NONE,
+                                           NULL,
+                                           &error);
+    if (error)
+        g_error("Failed to query directory contents.");
+
+    while (info = g_file_enumerator_next_file(enumerator, NULL, &error)) {
+        char *name;
+        guint64 size;
+
+        if (error)
+            g_error("Failed to query a file.");
+
+        name = g_file_info_get_attribute_as_string(info,
+                                                   G_FILE_ATTRIBUTE_STANDARD_NAME);
+        size = g_file_info_get_attribute_uint64(info,
+                                                G_FILE_ATTRIBUTE_STANDARD_SIZE);
+
+        clutter_model_append(model, COLUMN_NAME, name, COLUMN_SIZE, size, -1);
+    }
+
+    return model;
+}
 
 int main (int argc, char **argv)
 {
     ClutterActor *stage;
+    ClutterModel *model;
     NbtkWidget *box;
     NbtkWidget *entry;
+    NbtkWidget *list_view;
+    NbtkWidget *scroll_view;
 
     clutter_init(&argc, &argv);
 
@@ -39,6 +90,22 @@ int main (int argc, char **argv)
     clutter_container_add(CLUTTER_CONTAINER(box), CLUTTER_ACTOR(entry), NULL);
     clutter_container_child_set(CLUTTER_CONTAINER(box), CLUTTER_ACTOR(entry),
                                 "x-fill", TRUE, NULL);
+
+    scroll_view = nbtk_scroll_view_new();
+    clutter_container_add(CLUTTER_CONTAINER(box), CLUTTER_ACTOR(scroll_view), NULL);
+    clutter_container_child_set(CLUTTER_CONTAINER(box), CLUTTER_ACTOR(scroll_view),
+                                "x-fill", TRUE,
+                                "y-fill", TRUE,
+                                NULL);
+
+    model = create_model();
+
+    list_view = nbtk_list_view_new();
+    nbtk_list_view_set_item_type(NBTK_LIST_VIEW(list_view), NBTK_TYPE_LABEL);
+    nbtk_box_layout_set_vertical(NBTK_BOX_LAYOUT(list_view), TRUE);
+    nbtk_list_view_set_model(NBTK_LIST_VIEW(list_view), model);
+    nbtk_list_view_add_attribute(NBTK_LIST_VIEW(list_view), "text", COLUMN_NAME);
+    clutter_container_add(CLUTTER_CONTAINER(scroll_view), CLUTTER_ACTOR(list_view), NULL);
 
     clutter_actor_show_all(stage);
 
