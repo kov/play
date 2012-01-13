36e98ea64cbaca73f094d1386b1d7c6ef694bd3b
Gustavo Noronha Silva <gns@gnome.org>
2012-01-13 15:39:21 -0200

    Beautification

--- nodejs/chat.js
+++ nodejs/chat.js
@@ -4,12 +4,10 @@ var net = require('net');
 
 var clients = [];
 
-net.createServer(function (c) {
-
+function new_connection (c) {
     clients.push(c);
 
     c.on('data', function (data) {
-
         for (i = 0; i < clients.length; i++) {
             var client = clients[i];
 
@@ -18,12 +16,12 @@ net.createServer(function (c) {
 
             client.write(data);
         }
-
     });
 
     c.on('end', function () {
         var index = clients.indexOf(c);
         clients.splice(index, 1);
     });
+}
 
-}).listen(8081, '127.0.0.1')
+net.createServer(new_connection).listen(8081, '127.0.0.1')
