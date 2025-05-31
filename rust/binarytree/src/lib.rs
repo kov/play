d559d79b65b165d7ecf68ed776570dee2ea3f45d
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2025-05-31 11:29:12 -0300

    Implement remove for the binary tree
    This was a lot more complex than I thought. Ended up refactoring how
    insert works as well while doing it, and fixing an issue with the
    length book keeping. CoPilot helped with some tests, but got completely
    lost on the remove() implementation xD

--- rust/binarytree/src/lib.rs
+++ rust/binarytree/src/lib.rs
@@ -13,12 +13,6 @@ enum BalanceAction {
     RotateRight,
 }
 
-enum AtKeyAction {
-    Nothing,
-    AddNode,
-    RemoveNode,
-}
-
 struct Node<K, V> {
     key: K,
     value: V,
@@ -55,11 +49,15 @@ impl<K, V> Node<K, V> {
         }
     }
 
-    fn check_balance(&self) -> BalanceAction {
+    fn calculate_balance_factor(&self) -> isize {
         let left_height = self.left.as_ref().map_or(0, |n| n.height);
         let right_height = self.right.as_ref().map_or(0, |n| n.height);
 
-        let balance_factor = left_height as isize - right_height as isize;
+        left_height as isize - right_height as isize
+    }
+
+    fn check_balance(&self) -> BalanceAction {
+        let balance_factor = self.calculate_balance_factor();
 
         if balance_factor < -1 {
             BalanceAction::RotateLeft
@@ -89,50 +87,181 @@ impl<K, V> BinaryTree<K, V> {
     {
         if self.root.is_none() {
             self.root = Some(Node::new(key, value));
+
+            assert_eq!(
+                self.length, 0,
+                "Tree length should be 0 when inserting the root node"
+            );
+
+            self.length = 1;
+
             return;
         }
 
-        let action = self.at_key_mut(key, move |key, node, visited_nodes| {
-            if let Some(node) = node {
-                assert!(key == node.key);
-                node.value = value;
+        let (mut node, mut visited_nodes) = self.find_node_mut(&key);
+        if let Some(node) = node {
+            assert!(key == node.key);
+            node.value = value;
+        } else {
+            *node = Some(Node::new(key, value));
 
-                AtKeyAction::Nothing
-            } else {
-                *node = Some(Node::new(key, value));
+            self.rebalance_parents(&visited_nodes);
 
-                // After insert, go through all the nodes we visited updating their heights and
-                // checking their balance, rotating as necessary.
-                for &node_ptr in visited_nodes.iter().rev() {
-                    // Safety: the pointers we collected are of Options we went through when visiting
-                    // nodes to get here. We have exclusive access to the tree (&mut self) throughout
-                    // the whole process. The tree owns these values. Nothing we do moves or destroys
-                    // the Options, so these are safe to dereference.
-                    let node: &mut Option<Box<Node<K, V>>> = unsafe { &mut *node_ptr };
+            self.length += 1;
+        }
+    }
 
-                    let action = if let Some(node) = node {
-                        node.update_height();
-                        node.check_balance()
-                    } else {
-                        unreachable!("These nodes can't be None, as we walked through them")
-                    };
+    fn remove(&mut self, key: &K) -> Option<V>
+    where
+        K: Ord,
+    {
+        let (mut link, mut visited) = self.find_node_mut(key);
 
-                    match action {
-                        BalanceAction::RotateLeft => BinaryTree::rotate_left(node),
-                        BalanceAction::RotateRight => BinaryTree::rotate_right(node),
-                        BalanceAction::Nothing => (),
-                    }
+        if link.is_none() {
+            return None;
+        }
+
+        let mut removed = *link.take().unwrap();
+
+        // Calculate balance factor to decide which side to use.
+        let balance_factor = removed.calculate_balance_factor();
+
+        // Keep track of the nodes we visit while searching for successor / predecessor,
+        // so we can go up the tree.
+        let mut sub_visited = Vec::with_capacity(removed.height);
+        if removed.right.is_some() && (balance_factor < 0 || balance_factor == 0) {
+            let mut successor = &mut removed.right;
+
+            loop {
+                if successor.as_ref().unwrap().left.is_none() {
+                    break;
                 }
-
-                AtKeyAction::AddNode
+                sub_visited.push(successor as *mut _);
+                successor = &mut successor.as_mut().unwrap().left;
             }
-        });
 
-        match action {
-            AtKeyAction::AddNode => self.length += 1,
-            AtKeyAction::RemoveNode => unreachable!(),
-            AtKeyAction::Nothing => (),
-        };
+            let mut successor_node = successor.take();
+
+            // Put right-side child of successor in its place, if any.
+            *successor = successor_node.as_deref_mut().unwrap().right.take();
+
+            *link = successor_node;
+        } else if removed.left.is_some() {
+            let mut predecessor = &mut removed.left;
+
+            loop {
+                if predecessor.as_ref().unwrap().right.is_none() {
+                    break;
+                }
+                sub_visited.push(predecessor as *mut _);
+                predecessor = &mut predecessor.as_mut().unwrap().right;
+            }
+
+            let mut predecessor_node = predecessor.take();
+
+            // Put left-side child of predecessor in its place, if any.
+            *predecessor = predecessor_node.as_deref_mut().unwrap().left.take();
+
+            *link = predecessor_node;
+        }
+
+        // Move children of the removed node to its successor / predecessor. Keep in mind
+        // there may be no successor, if we are removing a leaf node.
+        if let Some(new_root) = link.as_deref_mut() {
+            // Sanity check.
+            assert!(new_root.left.is_none());
+            assert!(new_root.right.is_none());
+
+            new_root.left = removed.left.take();
+            new_root.right = removed.right.take();
+
+            // Add the new subtree root for rebalancing, as its height has also potentially changed.
+            visited.push(link as *mut _);
+        }
+
+        // And then go back up the tree rebalancing the parents, first of the removed node's
+        // original subtree, then of the new subtree root, then its parents.
+        if sub_visited.len() > 1 {
+            // Skip the first item, as that is the left or right child of the removed node,
+            // which is now None.
+            self.rebalance_parents(&sub_visited[1..]);
+        }
+        self.rebalance_parents(&visited);
+
+        self.length -= 1;
+
+        Some(removed.value)
+    }
+
+    fn rebalance_parents(&mut self, nodes: &[*mut Option<Box<Node<K, V>>>]) {
+        // After insert, go through all the nodes we visited updating their heights and
+        // checking their balance, rotating as necessary.
+        for &node_ptr in nodes.iter().rev() {
+            // Safety: the pointers we collected are of Options we went through when visiting
+            // nodes to get here. We have exclusive access to the tree (&mut self) throughout
+            // the whole process. The tree owns these values. Nothing we do moves or destroys
+            // the Options we keep track of here, so these are safe to dereference.
+            //
+            // NOTE: to ensure these invariants find_node_mut() must make sure to not include
+            // the node it returns independently, as that node may be moved before this
+            // funciton is called.
+            let node: &mut Option<Box<Node<K, V>>> = unsafe { &mut *node_ptr };
+
+            let action = if let Some(node) = node {
+                node.update_height();
+                node.check_balance()
+            } else {
+                unreachable!("These nodes can't be None, as we walked through them")
+            };
+
+            match action {
+                BalanceAction::RotateLeft => BinaryTree::rotate_left(node),
+                BalanceAction::RotateRight => BinaryTree::rotate_right(node),
+                BalanceAction::Nothing => (),
+            }
+        }
+    }
+
+    fn find_node_mut(
+        &mut self,
+        key: &K,
+    ) -> (
+        &mut Option<Box<Node<K, V>>>,
+        Vec<*mut Option<Box<Node<K, V>>>>,
+    )
+    where
+        K: Ord,
+    {
+        let mut node = &mut self.root;
+        let mut visited_nodes = vec![];
+        loop {
+            if node.is_none() {
+                break;
+            }
+
+            // Keep track of the pointer before we change it as...
+            let visited = node as *mut _;
+
+            let ordering = key.cmp(&node.as_ref().unwrap().key);
+            match ordering {
+                Ordering::Less => {
+                    node = &mut node.as_mut().unwrap().left;
+                }
+                Ordering::Greater => node = &mut node.as_mut().unwrap().right,
+                Ordering::Equal => {
+                    break;
+                }
+            }
+
+            // ... we will need to revisit the nodes we went through to update their height
+            // tracking and potentially rotate, in case of unbalance. We push after the
+            // ordering check to ensure we do not add the node we return independently,
+            // which would be a safety hazard, as that node may be moved by the caller of
+            // find_node_mut().
+            visited_nodes.push(visited);
+        }
+
+        (node, visited_nodes)
     }
 
     fn get(&self, key: &K) -> Option<&V>
@@ -155,42 +284,6 @@ impl<K, V> BinaryTree<K, V> {
             }
         }
         None
-    }
-
-    fn at_key_mut(
-        &mut self,
-        key: K,
-        func: impl FnOnce(
-            K,
-            &mut Option<Box<Node<K, V>>>,
-            &Vec<*mut Option<Box<Node<K, V>>>>,
-        ) -> AtKeyAction,
-    ) -> AtKeyAction
-    where
-        K: Ord,
-    {
-        let mut node = &mut self.root;
-        let mut visited_nodes = vec![];
-        loop {
-            if node.is_none() {
-                return func(key, node, &visited_nodes);
-            }
-
-            // We will need to revisit the nodes we went through to update
-            // their height tracking and potentially rotate, in case of unbalance.
-            visited_nodes.push(node as *mut _);
-
-            let ordering = key.cmp(&node.as_ref().unwrap().key);
-            match ordering {
-                Ordering::Less => {
-                    node = &mut node.as_mut().unwrap().left;
-                }
-                Ordering::Greater => node = &mut node.as_mut().unwrap().right,
-                Ordering::Equal => {
-                    return func(key, node, &visited_nodes);
-                }
-            }
-        }
     }
 
     fn rotate_left(node: &mut Option<Box<Node<K, V>>>) {
@@ -340,10 +433,19 @@ mod tests {
     #[test]
     fn test_basics() {
         let mut binary_tree = BinaryTree::new();
+        assert_eq!(binary_tree.len(), 0);
         binary_tree.insert(format!("kov"), 42u64);
+        assert_eq!(binary_tree.len(), 1);
         binary_tree.insert(format!("lala"), 13);
+        assert_eq!(binary_tree.len(), 2);
+        binary_tree.insert(format!("qwe"), 10);
+        assert_eq!(binary_tree.len(), 3);
         assert_eq!(binary_tree.get(&format!("kov")), Some(&42));
         assert_eq!(binary_tree.get(&format!("lala")), Some(&13));
+        assert_eq!(binary_tree.get(&format!("qwe")), Some(&10));
+        let qwe = binary_tree.remove(&format!("qwe"));
+        assert_eq!(qwe, Some(10));
+        assert_eq!(binary_tree.len(), 2);
         assert_eq!(binary_tree.get(&format!("qwe")), None);
     }
 
@@ -352,6 +454,7 @@ mod tests {
         let mut tree = BinaryTree::new();
         for i in 0..100 {
             tree.insert(i, i);
+            assert_eq!(tree.len(), (i + 1) as usize);
         }
         assert!(is_balanced(&tree.root));
         let keys = in_order_keys(&tree);
@@ -363,6 +466,7 @@ mod tests {
         let mut tree = BinaryTree::new();
         for i in (0..100).rev() {
             tree.insert(i, i);
+            assert_eq!(tree.len(), 100 - i as usize);
         }
         assert!(is_balanced(&tree.root));
         let keys = in_order_keys(&tree);
@@ -373,8 +477,9 @@ mod tests {
     fn test_balanced_after_mixed_inserts() {
         let mut tree = BinaryTree::new();
         let data = [10, 20, 5, 15, 25, 2, 7, 12, 17, 22, 27];
-        for &x in &data {
+        for (i, &x) in data.iter().enumerate() {
             tree.insert(x, x);
+            assert_eq!(tree.len(), i + 1);
         }
         assert!(is_balanced(&tree.root));
         let mut sorted = data.to_vec();
@@ -406,6 +511,7 @@ mod tests {
                 })),
             })),
         }));
+        tree.length = 3;
 
         // Assert initial structure and unbalance
         let root = tree.root.as_ref().unwrap();
@@ -442,15 +548,13 @@ mod tests {
     #[test]
     fn test_iterator_in_order() {
         let mut tree = BinaryTree::new();
-
         let mut data = vec![15, 3, 20, 7, 1, 18, 25, 5, 10];
-
-        for &x in &data {
+        for (i, &x) in data.iter().enumerate() {
             tree.insert(x, x * 2);
+            assert_eq!(tree.len(), i + 1);
         }
-
-        // Sort our reference vector.
         data.sort();
+        assert_eq!(tree.len(), data.len());
 
         // Create iterators with matching structure and compare them.
         let expected = data.iter().map(|&k| (k, k * 2));
@@ -461,14 +565,13 @@ mod tests {
     #[test]
     fn test_iterator_mut_in_order_and_mutation() {
         let mut tree = BinaryTree::new();
-
         let mut data = vec![15, 3, 20, 7, 1, 18, 25, 5, 10];
-
-        for &x in &data {
+        for (i, &x) in data.iter().enumerate() {
             tree.insert(x, x * 2);
+            assert_eq!(tree.len(), i + 1);
         }
-
         data.sort();
+        assert_eq!(tree.len(), data.len());
 
         // First, mutate all values via the mutable iterator
         for (k, v) in tree.iter_mut() {
@@ -479,5 +582,57 @@ mod tests {
         let expected = data.iter().map(|&k| (k, k * 2 + 1000));
         let actual = tree.iter().map(|(k, v)| (*k, *v));
         assert!(expected.eq(actual));
+    }
+
+    #[test]
+    fn test_remove_node_with_two_children() {
+        let mut tree = BinaryTree::new();
+
+        // Construct a tree where 10 has two children
+        for (i, &k) in [10, 5, 15, 3, 7, 12, 18].iter().enumerate() {
+            tree.insert(k, k * 10);
+            assert_eq!(tree.len(), i + 1);
+        }
+
+        // Remove node with two children (10)
+        let removed = tree.remove(&10);
+        assert_eq!(removed, Some(100));
+        assert_eq!(tree.len(), 6);
+        // 10 should be gone, all others present
+        for &k in &[5, 15, 3, 7, 12, 18] {
+            assert_eq!(tree.get(&k), Some(&(k * 10)));
+        }
+
+        assert_eq!(tree.get(&10), None);
+
+        // Tree remains balanced
+        assert!(is_balanced(&tree.root));
+
+        // In-order keys are correct
+        let keys = in_order_keys(&tree);
+        assert_eq!(keys, vec![3, 5, 7, 12, 15, 18]);
+    }
+
+    #[test]
+    fn test_remove_all_nodes_one_by_one() {
+        let mut tree = BinaryTree::new();
+        let data = [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15];
+        for (i, &k) in data.iter().enumerate() {
+            tree.insert(k, k);
+            assert_eq!(tree.len(), i + 1);
+        }
+
+        assert_eq!(tree.len(), data.len());
+
+        for (i, &k) in data.iter().enumerate() {
+            let removed = tree.remove(&k);
+            assert_eq!(removed, Some(k));
+            assert_eq!(tree.len(), data.len() - i - 1);
+            assert!(is_balanced(&tree.root));
+            assert_eq!(tree.get(&k), None);
+        }
+
+        assert_eq!(tree.len(), 0);
+        assert!(tree.root.is_none());
     }
 }
